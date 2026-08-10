4295d30286cdb7b19641095ce31ead25703f0806
fari0n4ik <cacalox51@gmail.com>
2026-08-10 18:55:57 +0700

    Create Activity.kt

---
+++ app/src/main/java/com/example/lockApp/Main/Activity.kt
@@ -0,0 +1,17 @@
+package com.example.lockapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.startBtn).setOnClickListener {
+            startService(Intent(this, LockService::class.java))
+            finish()
+        }
+    }
+}
